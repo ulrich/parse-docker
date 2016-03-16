947e9e84b59a6ac0496ff8a8c7537a2d6fda7741
Didier Franc <mail@didierfranc.com>
2016-03-16 23:24:53 +0100

    Update parse-server.sh

--- parse-server.sh
+++ parse-server.sh
@@ -9,6 +9,8 @@ echo '{
   ]
 }' > ../parse-dashboard/Parse-Dashboard/parse-dashboard-config.json
 
+export PARSE_DASHBOARD_ALLOW_INSECURE_HTTP=1
+
 mongod --dbpath /db & 
 parse-server & 
 cd parse-dashboard && npm run dashboard
