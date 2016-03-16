fe6d81a77f75b5a6fff6e0725f1c905832b3465b
Didier Franc <mail@didierfranc.com>
2016-03-17 00:00:31 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+# MongoDB
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.2 main" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-get update && apt-get install -y mongodb-org && mkdir /db
+
+# Parse-Server
+npm install -g parse-server
+
+# Parse-Dashboard
+git clone https://github.com/ParsePlatform/parse-dashboard.git
+cd parse-dashboard && npm install
+npm build
