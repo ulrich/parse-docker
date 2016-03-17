fdb1c521408f34801ee35819f8022a4fa0b0a885
Didier Franc <mail@didierfranc.com>
2016-03-17 11:30:31 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 # MongoDB
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.2 main" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-apt-get update && apt-get install -y mongodb-org dnsutils curl git && mkdir /db
+apt-get update && apt-get install -y mongodb-org dnsutils curl git build-essential && mkdir /db
 
 # NodeJS
 curl -sL https://deb.nodesource.com/setup_4.x | bash -
