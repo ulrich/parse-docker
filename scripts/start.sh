7d86c8e2a6535b49099ff4dca6ebec0e7511e5bd
Didier Franc <mail@didierfranc.com>
2016-03-17 19:18:28 +0100

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -28,8 +28,8 @@ config=${config/key/$PARSE_SERVER_MASTER_KEY}
 config=${config/dashuser/$PARSE_DASH_USER}
 config=${config/dashpass/$PARSE_DASH_PASS}
 
-echo $config > parse-dashboard/config.json
+echo $config > config.json
 
 mongod --dbpath /db & 
 parse-server & 
-cd parse-dashboard && npm start -- --config config.json --port 4040 --allowInsecureHTTP=1
+parse-dashboard --config config.json --port 4040 --allowInsecureHTTP=1
