be893fdb01f874830413eba21bc1589199d599a3
Didier Franc <mail@didierfranc.com>
2016-03-17 11:40:52 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.2 main" | tee 
 apt-get update && apt-get install -y mongodb-org dnsutils curl git build-essential && mkdir /db
 
 # NodeJS
-curl -sL https://deb.nodesource.com/setup_4.x | bash -
+curl -sL https://deb.nodesource.com/setup_5.x | bash -
 apt-get install -y nodejs
 
 # Parse-Server
@@ -13,4 +13,4 @@ npm install -g parse-server
 # Parse-Dashboard
 git clone https://github.com/ParsePlatform/parse-dashboard.git
 cd parse-dashboard && npm install
-npm run prepublish
+npm run build
